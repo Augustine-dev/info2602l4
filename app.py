88eb0c314ff47fe77e0cade9200c64db38712394
Augustine-dev <157492187+Augustine-dev@users.noreply.github.com>
2025-03-26 02:36:59 -0400

    Task 5

--- app.py
+++ app.py
@@ -151,6 +151,29 @@ def create_todo_action():
   flash('Created')
   return redirect(url_for('todos_page'))
 
+@app.route('/toggle/<id>', methods=['POST'])
+@jwt_required()
+def toggle_todo_action(id):
+  todo = current_user.toggle_todo(id)
+  if todo is None:
+    flash('Invalid id or unauthorized')
+  else:
+    flash(f'Todo { "done" if todo.done else "not done" }!')
+  return redirect(url_for('todos_page'))
+
+@app.route('/editTodo/<id>', methods=["POST"])
+@jwt_required()
+def edit_todo_action(id):
+  data = request.form
+  res = current_user.update_todo(id, data["text"])
+  if res:
+    flash('Todo Updated!')
+  else:
+    flash('Todo not found or unauthorized')
+  return redirect(url_for('todos_page'))
+
+
+
 
 
 
